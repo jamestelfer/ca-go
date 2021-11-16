442fae93b1f29cc523911b5c62d2ed3a749ee9b6
James Liu <disposedtrolley@users.noreply.github.com>
2021-11-16 05:18:28 +0000

    Add basic client config, interface, and tests

---
+++ flags/flags_test.go
@@ -0,0 +1,28 @@
+package flags_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cultureamp/ca-go/flags"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewConfig(t *testing.T) {
+	t.Run("errors if an SDK key is not supplied", func(t *testing.T) {
+		_, err := flags.NewConfig()
+		require.Error(t, err)
+	})
+
+	t.Run("does not error if SDK key supplied as env var", func(t *testing.T) {
+		os.Setenv("LAUNCHDARKLY_SDK_KEY", "foobar")
+		_, err := flags.NewConfig()
+		require.NoError(t, err)
+	})
+
+	t.Run("does not error if SDK key supplied as config option", func(t *testing.T) {
+		_, err := flags.NewConfig(flags.WithSDKKey("foobar"))
+		require.NoError(t, err)
+	})
+
+}
