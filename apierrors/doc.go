61e086fe930a0d6ac7178dfeeb529301035e60d0
James Telfer <792299+jamestelfer@users.noreply.github.com>
2021-11-17 00:34:34 +1100

    WIP

---
+++ apierrors/doc.go
@@ -0,0 +1,4 @@
+// The apierrors package implements API error responses as per the JSON:API spec, with helpers for creating the messages
+//
+// See: https://jsonapi.org/examples/#error-objects
+package apierrors
---
+++ apierrors/response_test.go
@@ -0,0 +1,45 @@
+package apierrors_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cultureamp/ca-go/apierrors"
+	"github.com/cultureamp/ca-go/ref"
+	"github.com/stretchr/testify/assert"
+)
+
+func ExampleResponse_AddError() {
+	er := apierrors.Response{}.AddError("title")
+
+	fmt.Println(string(er.MustSerialize()))
+	// Output:
+	// {"errors":[{"title":"title",description":"description","id":"id"}]}
+}
+
+func ExampleResponse_Add() {
+	er := apierrors.Response{}.Add(apierrors.ErrorInstance{
+		Title:  "Unexpected error",
+		Detail: ref.String("Further error details (without including internal information)"),
+		ID:     ref.String("Request ID or similar"),
+	})
+
+	fmt.Println(string(er.MustSerialize()))
+	// Output:
+	// {"errors":[{"title":"Unexpected error","detail":"Further error details (without including internal information)","id":"Request ID or similar"}]}
+}
+
+func ExampleNewUnexpectedError() {
+	er := apierrors.NewUnexpectedError(ref.String("Request ID or similar"))
+
+	fmt.Println(string(er.MustSerialize()))
+	// Output:
+	// {"errors":[{"title":"Unexpected error","id":"Request ID or similar"}]}
+}
+
+func TestResponse_Add(t *testing.T) {
+	er := apierrors.Response{}.AddError("500")
+	assert.NotNil(t, er)
+	assert.Len(t, er.Errors, 1)
+	assert.Equal(t, apierrors.ErrorInstance{Title: "500"}, er.Errors[0])
+}
