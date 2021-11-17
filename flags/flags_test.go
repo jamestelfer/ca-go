02fabd08539ec48afdb1d61621e22f2afbe3694d
James Liu <disposedtrolley@users.noreply.github.com>
2021-11-17 03:24:55 +0000

    Improve SDK initialisation
    Restructures the wrapper's interface to allow for two modes of usage;
    as an internally-managed singleton through the exported functions
    `Configure`, `Connect`, and `GetDefaultClient`, or as a self-managed
    object by calling the `NewClient` constructor.
    
    Also separates the configuration and connection phases by introducing
    a `Connect` method. This enables consumers greater control over
    when configuration is supplied to the SDK and when a live connection
    is established.

--- flags/flags_test.go
+++ flags/flags_test.go
@@ -9,25 +9,55 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewConfig(t *testing.T) {
+func TestInitialisationSingleton(t *testing.T) {
 	t.Run("errors if an SDK key is not supplied", func(t *testing.T) {
-		_, err := flags.NewConfig()
+		err := flags.Configure()
+		require.Error(t, err)
+
+		_, err = flags.GetDefaultClient()
 		require.Error(t, err)
 	})
 
 	t.Run("does not error if SDK key supplied as env var", func(t *testing.T) {
 		os.Setenv("LAUNCHDARKLY_SDK_KEY", "foobar")
-		_, err := flags.NewConfig()
+		defer os.Unsetenv("LAUNCHDARKLY_SDK_KEY")
+		err := flags.Configure()
 		require.NoError(t, err)
 	})
 
 	t.Run("does not error if SDK key supplied as config option", func(t *testing.T) {
-		_, err := flags.NewConfig(flags.WithSDKKey("foobar"))
+		err := flags.Configure(flags.WithSDKKey("foobar"))
 		require.NoError(t, err)
 	})
 
 	t.Run("allows an initialisation wait time to be specified", func(t *testing.T) {
-		_, err := flags.NewConfig(
+		err := flags.Configure(
+			flags.WithSDKKey("foobar"),
+			flags.WithInitWait(2*time.Second))
+		require.NoError(t, err)
+	})
+}
+
+func TestInitialisationClient(t *testing.T) {
+	t.Run("errors if an SDK key is not supplied", func(t *testing.T) {
+		_, err := flags.NewClient()
+		require.Error(t, err)
+	})
+
+	t.Run("does not error if SDK key supplied as env var", func(t *testing.T) {
+		os.Setenv("LAUNCHDARKLY_SDK_KEY", "foobar")
+		defer os.Unsetenv("LAUNCHDARKLY_SDK_KEY")
+		_, err := flags.NewClient()
+		require.NoError(t, err)
+	})
+
+	t.Run("does not error if SDK key supplied as config option", func(t *testing.T) {
+		_, err := flags.NewClient(flags.WithSDKKey("foobar"))
+		require.NoError(t, err)
+	})
+
+	t.Run("allows an initialisation wait time to be specified", func(t *testing.T) {
+		_, err := flags.NewClient(
 			flags.WithSDKKey("foobar"),
 			flags.WithInitWait(2*time.Second))
 		require.NoError(t, err)
