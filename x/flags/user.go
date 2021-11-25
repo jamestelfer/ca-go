43076be09e78680577036266533c8fb5578b251d
James Liu <james.liu@cultureamp.com>
2021-11-25 16:54:50 +1100

    Add alternate query funcs that extract the user from context

--- x/flags/user.go
+++ x/flags/user.go
@@ -1,18 +1,24 @@
 package flags
 
 import (
+	"context"
+	"errors"
+
+	"github.com/cultureamp/ca-go/request"
 	"gopkg.in/launchdarkly/go-sdk-common.v2/lduser"
 	"gopkg.in/launchdarkly/go-sdk-common.v2/ldvalue"
 )
 
 const (
-	anonymousUser                   = "ANONYMOUS_USER"
-	userAttributeAccountAggregateID = "accountAggregateId"
+	anonymousUser                    = "ANONYMOUS_USER"
+	userAttributeAccountAggregateID  = "accountAggregateID"
+	userAttributeRealUserAggregateID = "realUserAggregateID"
 )
 
 type User struct {
-	userAggregateID    string
-	accountAggregateID string
+	effectiveUserAggregateID string
+	realUserAggregateID      string
+	accountAggregateID       string
 
 	ldUser lduser.User
 }
@@ -25,28 +31,51 @@ func WithAccountAggregateID(id string) UserOption {
 	}
 }
 
+func WithRealUserAggregateID(id string) UserOption {
+	return func(u *User) {
+		u.realUserAggregateID = id
+	}
+}
+
 func NewAnonymousUser() User {
 	return User{
 		ldUser: lduser.NewAnonymousUser(anonymousUser),
 	}
 }
 
-func NewUser(userAggregateID string, opts ...UserOption) User {
+func NewUser(effectiveUserAggregateID string, opts ...UserOption) User {
 	u := &User{
-		userAggregateID: userAggregateID,
+		effectiveUserAggregateID: effectiveUserAggregateID,
 	}
 
 	for _, opt := range opts {
 		opt(u)
 	}
 
-	u.ldUser = lduser.NewUserBuilder(userAggregateID).Custom(
+	userBuilder := lduser.NewUserBuilder(effectiveUserAggregateID)
+	userBuilder.Custom(
 		userAttributeAccountAggregateID,
-		ldvalue.String(u.accountAggregateID)).Build()
+		ldvalue.String(u.accountAggregateID))
+	userBuilder.Custom(
+		userAttributeRealUserAggregateID,
+		ldvalue.String(u.realUserAggregateID))
+	u.ldUser = userBuilder.Build()
 
 	return *u
 }
 
 func (u User) RawUser() interface{} {
 	return u.ldUser
+}
+
+func UserFromContext(ctx context.Context) (User, error) {
+	authenticatedUser, ok := request.AuthenticatedUserFromContext(ctx)
+	if !ok {
+		return User{}, errors.New("no AuthenticatedUser in supplied context")
+	}
+
+	return NewUser(
+		authenticatedUser.EffectiveUserAggregateID,
+		WithAccountAggregateID(authenticatedUser.AccountAggregateID),
+		WithRealUserAggregateID(authenticatedUser.RealUserAggregateID)), nil
 }
