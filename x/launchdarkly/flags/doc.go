84cd3cc5630b870ba8e162c110b26d23f1dc9ea0
James Liu <james.liu@cultureamp.com>
2022-02-01 13:59:44 +1100

    docs: fix whitespace issues and add info on toggles

--- x/launchdarkly/flags/doc.go
+++ x/launchdarkly/flags/doc.go
@@ -1,6 +1,6 @@
-// Package flags provides access to feature flags. It wraps the LaunchDarkly SDK
-// to expose a convenient and consistent way of configuring and using the
-// client.
+// Package flags provides access to feature flags and product toggles. It wraps
+// the LaunchDarkly SDK and exposes a convenient and consistent way of configuring
+// and using the client.
 //
 // The client can be configured and used as a managed singleton or as an
 // instance returned from a constructor function. The managed singleton provides
@@ -17,16 +17,16 @@
 //
 //   err = flags.Connect()
 //   if err != nil {
-//	   // handle errors connecting to LaunchDarkly
-//	 }
+//     // handle errors connecting to LaunchDarkly
+//   }
 //
 // To configure the client as a instance that you manage:
 //   client, err := flags.NewClient(
 //	   flags.WithSDKKey("foobar"),
 //   )
 //   if err != nil {
-//	   // handle invalid configuration
-//	 }
+//     // handle invalid configuration
+//   }
 //
 // The client can be configured to use the Relay Proxy in either Daemon or Proxy
 // modes. See WithDaemonMode and WithProxyMode for more information.
@@ -34,30 +34,35 @@
 // Querying for flags is done on the client instance. You can get instance from the
 // managed singleton with GetDefaultClient():
 //   client, err := flags.GetDefaultClient()
-//	 if err != nil {
-//	   // client not configured or connected
-//	 }
+//   if err != nil {
+//     // client not configured or connected
+//   }
 //
 // A typical query takes three pieces of data:
 // 1) The flag name (the "key" within the LaunchDarkly UI).
-// 2) The evaluation context, currently limited to a User object. This context
-//    contains the identifiers and attributes that flags are configured to target
-//    against.
+// 2) The evaluation context, which contains the identifiers and attributes of an
+//    entity that you wish to query the state of a flag for.
+//    There are two kinds of evaluation contexts:
+//      - Flag
+//      - Toggle
+//    Currently, the Flag kind supports a User context. The Toggle kind supports
+//    an Account context. See the evaluationcontext package for more information.
 // 3) The fallback value to return if an evaluation error occurs. This value will
 //    always be reflected as the value of the flag if err is not nil.
 //
-// In most cases, the client can automatically extract the User from the request
-// context:
-//   val, err := client.QueryBool(ctx, "my-flag", false)
+// In most cases, the client can automatically build the evaluation context from
+// the request context:
+//   flagVal, err := client.QueryBool(ctx, "flag.my-flag", false)
 //
-// You can also supply your own User as an evaluation context:
+//   toggleVal, err := client.QueryToggle(ctx, "toggle.my-toggle", false)
+//
+// You can also supply your own evaluation context:
 //   user := flags.NewUser(
-//			   "user-id",
-//			   flags.WithAccountID("account-id"),
-//	 )
-//   // user := flags.AnonymousUser() // if unauthenticated
+//             "user-id",
+//             flags.WithAccountID("account-id"),
+//   )
 //
-//   val, err := client.QueryBoolWithEvaluationContext("my-flag", user, false)
+//   val, err := client.QueryBoolWithEvaluationContext("flag.my-flag", user, false)
 //
 // When your application is shutting down, you should call Shutdown() to gracefully
 // close connections to LaunchDarkly:
